b06c1dea7d0008f68d551b5ebf3901c2cbbee0d2
Mark Marklu Minhthy <marklu87@gmail.com>
2017-10-14 13:46:40 -0600

    linked styling

--- friend_scripts/src/App.js
+++ friend_scripts/src/App.js
@@ -1,17 +1,17 @@
-import React, { Component } from 'react';
-
-import Main from './containers/main/index';
-import MyStories from './containers/myPage/myStories';
+import React, { Component } from "react";
+import Main from "./containers/main/index";
+import MyStories from "./containers/myPage/myStories";
+import "./App.css";
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-      <Main />
-      <MyStories />
-    </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				<Main />
+				<MyStories />
+			</div>
+		);
+	}
 }
 
 export default App;
