71807fdac53cd438415f9706ec91d5ccfee3c8c6
ccruml20 <ccrumley1@gmail.com>
2017-10-21 06:26:47 -0600

    My Computer Really

--- friend_scripts/src/containers/Landing/Landing_Main.js
+++ friend_scripts/src/containers/Landing/Landing_Main.js
@@ -16,30 +16,33 @@ export default class Landing_Main extends React.Component {
         <div style={{ backgroundColor: '#2f767d', padding: '0' , height: '75%'}} className="col-md-12">
           <img style={{paddingBottom: '20px'}}  className="img-responsive" src={BackgroundImg} alt="" />
           <div className='col-md-12' style={{backgroundColor: 'white', padding: '0' , height: '80%'}}>
-            <div style={{margin: '20px 0px 20px 0'}} className='col-md-3'><LoginForm {...this.props} /></div>
-            <div style={{margin: '20px 200px 20px 0'}} className='col-md-6'> <img   className="img-responsive" src={arrows} alt="" /></div>
+            <div style={{margin: '20px 100px 20px 0'}} className='col-md-3'><LoginForm {...this.props} /></div>
+            {/* <div style={{margin: '20px 60px 20px 0px'}} className='col-md-6'> <img   className="img-responsive" src={arrows} alt="" /></div> */}
+            <div className='col-md-8'>
+              <h1>Log in through FACEBOOK to start building Stories with Yo Friends!</h1>
+            </div>
           </div>
         </div>
           <div style={{textAlign: 'center', color:'white', backgroundColor: '#143f48', height: '20vh'}} className='col-md-12'>
             {/* <div className='row'> */}
               <div className='col-md-12'>
                 <div className='col-md-4'>
-                  <h2  style={{fontSize: '2em'}} className='col-md-12' ><strong>Join</strong>   Stories</h2>
+                  <h2  style={{fontSize: '3em'}} className='col-md-12' ><strong>Join</strong>   Stories</h2>
                   <br></br>
                   <br></br>
-                  <h2 style={{paddingTop: '30px'}} >Join in on a Friend Scripts story to collaborate with other writers and editors, to just play around or take your ideas to the next level.</h2>
+                  <p style={{color:'rgba(255, 255, 255, 0.50)', fontSize: '20px', paddingTop: '30px'}} >Join in on a Friend Scripts story to collaborate with other writers and editors, to just play around or take your ideas to the next level.</p>
                 </div>
                 <div className='col-md-4'>
-                    <h1 style={{fontSize: '2em'}} className='col-md-12' ><strong>Chat</strong>   it <strong>up</strong> </h1>
+                    <h1 style={{fontSize: '3em'}} className='col-md-12' ><strong>Chat</strong>   it <strong>up</strong> </h1>
                     <br></br>
                     <br></br>
-                    <h2 style={{paddingTop: '30px'}}>Friend Scripts in app messenger makes it easy to communicate with other collaborators, to help keep the project on track.</h2>
+                    <p style={{color:'rgba(255, 255, 255, 0.50)', fontSize: '20px', paddingTop: '30px'}}>Friend Scripts in app messenger makes it easy to communicate with other collaborators, to help keep the project on track.</p>
                 </div>
                 <div className='col-md-4'>
-                    <h1 style={{fontSize: '2em'}} className='col-md-12' ><strong>Editor</strong>   Market Place</h1>
+                    <h1 style={{fontSize: '3em'}} className='col-md-12' ><strong>Editor</strong>   Market Place</h1>
                     <br></br>
                     <br></br>
-                    <h3 style={{paddingTop: '30px'}}>If you're an editor, take advantage of all of our current popular stories, maybe together your project can make it big</h3>
+                    <p style={{color:'rgba(255, 255, 255, 0.50)', fontSize: '20px', paddingTop: '30px'}}>If you're an editor, take advantage of all of our current popular stories, maybe together your project can make it big</p>
                   </div>
               </div>
             {/* </div> */}
